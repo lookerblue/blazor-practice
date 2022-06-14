18b3492e35d5615fbd6f1391319209d83cb9d3ec
lookerblue <lookerblue@hotmail.com>
2022-06-14 14:03:11 +0800

    .

---
+++ src/BlazorPractice.Client/Models/ExampleModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorPractice.Client
+{
+    public class ExampleModel
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string? Name { get; set; }
+    }
+}
