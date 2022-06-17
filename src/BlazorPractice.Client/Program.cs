64a8c43cacbcf4f6faca3eb9f0aa0ed524348f5d
lookerblue <lookerblue@hotmail.com>
2022-06-17 11:46:10 +0800

    .

--- src/BlazorPractice.Client/Program.cs
+++ src/BlazorPractice.Client/Program.cs
@@ -1,11 +1,17 @@
 using BlazorPractice.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using System;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// 
+builder.Services.AddHttpClient("WebAPI", client => client.BaseAddress = new Uri("https://localhost:7111/"));
 
 await builder.Build().RunAsync();
--- src/BlazorPractice.Server/Program.cs
+++ src/BlazorPractice.Server/Program.cs
@@ -1,13 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+// 更改 Api 响应的数据结构，使得响应结果只包含错误信息。
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            if (context.HttpContext.Request.Path == "/StarshipValidation")
+            {
+                return new BadRequestObjectResult(context.ModelState);
+            }
+            else
+            {
+                return new BadRequestObjectResult(new ValidationProblemDetails(context.ModelState));
+            }
+        };
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// 设置跨域访问 CORS
+app.UseCors(policy => policy
+    .WithOrigins("https://localhost:7111", "https://localhost:7172")
+    .AllowAnyMethod()
+    .WithHeaders(HeaderNames.ContentType));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
