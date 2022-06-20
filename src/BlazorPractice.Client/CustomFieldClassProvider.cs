f78131d061eeeaae2b15f227349caf98e458184d
lookerblue <lookerblue@hotmail.com>
2022-06-20 10:28:10 +0800

    .

---
+++ src/BlazorPractice.Client/CustomFieldClassProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorPractice.Client
+{
+    public class CustomFieldClassProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            var isValid = !editContext.GetValidationMessages(fieldIdentifier).Any();
+
+            return isValid ? "validField" : "invalidField";
+        }
+    }
+}
