29747ec7bc15b59403aa0634ba52b4a48ccd9866
lookerblue <lookerblue@hotmail.com>
2022-07-12 11:52:27 +0800

    .

---
+++ src/BlazorPractice.Client/Models/PanelBody.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorPractice.Client
+{
+    public class PanelBody
+    {
+        public string? Text { get; set; }
+        public string? Style { get; set; }
+    }
+}
---
+++ src/BlazorPractice.Client/wwwroot/BlazorPractice.Client.lib.module.js
@@ -0,0 +1,8 @@
+﻿
+export function beforeStart(options, extensions) {
+    console.log("beforeStart");
+}
+
+export function afterStarted(blazor) {
+    console.log("afterStarted");
+}
